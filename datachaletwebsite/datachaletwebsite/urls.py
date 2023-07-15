ede33066d807a037835c072b21ba146a138c5c7f
datachalet <datachalet@gmail.com>
2023-07-15 22:37:37 +0200

    hello world

--- datachaletwebsite/datachaletwebsite/urls.py
+++
@@ -1,9 +0,0 @@
-# your_app_name/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-]
-
